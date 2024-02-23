ce69f69d14496ab2271bf16c651d436246448ad7
Shogo Ogami <sg.tao.so@gmail.com>
2024-02-24 01:54:19 +0900

    Add:update vite.config.js for use path-alias

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  // for path alias
+  resolve: {
+    alias: {
+      '@/': `${__dirname}/src/`,
+      '~/': `${__dirname}/public/`,
+    },
+  },
 })
