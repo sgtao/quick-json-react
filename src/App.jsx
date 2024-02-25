cfecbe1204d7cefe3ba92dbc875cb6e5fefdd99d
Shogo Ogami <sg.tao.so@gmail.com>
2024-02-25 15:26:47 +0900

    Add:update App.jsx for remove default elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import viteLogo from '~/vite.svg';
-import '@/App.css';
-import TextInput from '@/components/TextInput';
+import './App.css';
+import TextInput from './components/TextInput';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,28 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>json-viewer</h1>
       <div className="card">
         <TextInput textInputState={[text, setText]} />
         <p>Entered Text: {text}</p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -5,7 +5,7 @@ import App from '../App';
 describe('Simple working test', () => {
   it('the title is visible', () => {
     render(<App />);
-    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+    expect(screen.getByText(/json-viewer/i)).toBeInTheDocument();
   });
 
   it('should increment count on click', async () => {
