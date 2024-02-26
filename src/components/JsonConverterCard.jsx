238977f459e36f82c0624bab9d17e6eef57a7b59
Shogo Ogami <sg.tao.so@gmail.com>
2024-02-26 09:05:00 +0900

    Add:add JsonConverterCard button for check input

--- src/components/JsonConverterCard.jsx
+++ src/components/JsonConverterCard.jsx
@@ -3,17 +3,32 @@ import { useState } from 'react';
 import TextInput from './TextInput';
 
 const JsonConverterCard = () => {
-  const [count, setCount] = useState(0);
   const [text, setText] = useState('');
+  const [checkResult, setCheckResult] = useState('');
+  const checkTextInput = () => {
+    setCheckResult('');
+    const checkJson = (text) => {
+      let resultCheck = '';
+      try {
+        JSON.parse(text);
+        resultCheck = 'JSON format';
+      } catch (e) {
+        resultCheck = 'Not JSON format';
+      }
+      return resultCheck;
+    };
+    setCheckResult(checkJson(text));
+  };
 
   return (
     <div>
       <div className="card">
         <TextInput textInputState={[text, setText]} />
-        <p>Entered Text: {text}</p>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p aria-label="Entered-Text">Entered Text: {text}</p>
+        <button aria-label="Check-JSON" onClick={checkTextInput}>
+          Check & Beautify
         </button>
+        <p aria-label="Check-Result">Check Result: {checkResult}</p>
       </div>
     </div>
   );
--- src/tests/JsonConverterCard.test.jsx
+++ src/tests/JsonConverterCard.test.jsx
@@ -1,11 +1,39 @@
-import { expect, describe, it } from 'vitest';
-import { render, screen, userEvent } from './utils/test-utils';
+import { expect, describe, test, it, vi } from 'vitest';
+import { cleanup, render, screen, userEvent } from './utils/test-utils';
 import JsonConverterCard from '@/components/JsonConverterCard';
 
 describe('JsonConverterCard test', () => {
+  test('TextInput should display the entered text', async () => {
+    // Arrange
+    const testText = 'Test Text';
+    render(<JsonConverterCard />);
+
+    // Act
+    const textInput = screen.getByLabelText('Text Input');
+    await userEvent.type(textInput, testText);
+    userEvent.click(screen.getByRole('button'));
+
+    // Assert
+    expect(textInput.value).toBe(testText);
+  });
+
+  test('TextInput with legal JSON should display JSON format', async () => {
+    // Arrange
+    const testText = '{{ "test": true \\}';
+    render(<JsonConverterCard />);
+
+    // Act
+    const textInput = screen.getByLabelText('Text Input');
+    await userEvent.type(textInput, testText);
+    userEvent.click(screen.getByRole('button'));
+
+    // Assert
+    expect(await screen.findByText('Check Result:')).toBeInTheDocument();
+  });
+
   it('should increment count on click', async () => {
     render(<JsonConverterCard />);
     userEvent.click(screen.getByRole('button'));
-    expect(await screen.findByText(/count is 1/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Check & Beautify/i)).toBeInTheDocument();
   });
 });
