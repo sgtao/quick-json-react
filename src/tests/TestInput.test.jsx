a81378c1a3ea06e5335d8b1d1a117ec40029323c
Shogo Ogami <sg.tao.so@gmail.com>
2024-03-02 13:11:58 +0900

    Fix:update tests/TextInput.test.js for coverage whole

--- src/tests/TestInput.test.jsx
+++ src/tests/TestInput.test.jsx
@@ -1,9 +1,28 @@
 // TextInput.test.jsx
-import { expect, describe, it, test, vi } from 'vitest';
+import { expect, describe, it, test, vi, beforeEach, afterAll } from 'vitest';
 import { render, screen, userEvent } from './utils/test-utils';
 import TextInput from '@/components/TextInput';
 
 describe('TextInput Component Test', async () => {
+  beforeEach(() => {
+    // jsdom が Clipboard API を実装していないのでダミー実装を用意する
+    Object.assign(navigator, {
+      clipboard: {
+        text: '',
+        readText() {
+          return Promise.resolve(this.text);
+        },
+        writeText(data) {
+          this.text = data;
+          return Promise.resolve();
+        },
+      },
+    });
+  });
+  afterAll(() => {
+    Object.assign(navigator, { clipboard: undefined });
+  });
+
   it('should render a text input', () => {
     const textInputState = ['', () => {}]; // Example state for testing
     render(<TextInput textInputState={textInputState} />);
@@ -40,20 +59,59 @@ describe('TextInput Component Test', async () => {
     expect(clearText).toHaveBeenCalledOnce();
   });
 
-  /*
-  test('PasteButton Clicked', async () => {
+  test('PasteButton Clicked when Clipboard has something', async () => {
     // Arrange
     const test = 'Hello World';
+    navigator.clipboard.writeText(test);
     const setText = vi.fn();
     // const handlePasteClick = vi.spyOn(TextInput, 'handlePasteClick');
-    render(<TextInput textInputState={[test, setText]} />);
+    render(<TextInput textInputState={['', setText]} />);
 
     // Act
     await userEvent.click(screen.getByRole('button', { name: 'Paste Button' }));
 
     // Assert
     // handlePasteClick が読み出されるか？を確認
-    expect(setText).toHaveBeenCalledOnce();
+    expect(setText).toHaveBeenCalledWith('Hello World');
   });
-*/
+});
+
+describe('TextInput Component Test in illegal case', async () => {
+  beforeEach(() => {
+    // jsdom が Clipboard API を実装していないのでダミー実装を用意する
+    const readText = vi.fn();
+    Object.assign(navigator, {
+      clipboard: {
+        text: '',
+        readText,
+        writeText(data) {
+          this.text = data;
+          return Promise.resolve();
+        },
+      },
+    });
+  });
+  afterAll(() => {
+    Object.assign(navigator, { clipboard: undefined });
+  });
+
+  test('PasteButton Clicked when Clipboard is blank.', async () => {
+    // Arrange
+    // const test = 'Hello World';
+    const setText = vi.fn();
+    navigator.clipboard.readText.mockRejectedValue(
+      new Error('clipboard-error'),
+    );
+    const alert = vi.fn();
+    Object.assign(window, { alert });
+
+    render(<TextInput textInputState={['', setText]} />);
+
+    // Act
+    await userEvent.click(screen.getByRole('button', { name: 'Paste Button' }));
+
+    // Assert
+    // window.alert が読み出されるか？を確認
+    expect(window.alert).toHaveBeenCalledOnce();
+  });
 });
